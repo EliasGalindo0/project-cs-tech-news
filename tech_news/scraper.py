949fb54d02751dd8c33ca91eeaa6737afda08b19
EliasGalindo0 <eliasgalindo@gmail.com>
2022-11-08 23:53:54 -0300

    feat: scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -18,7 +18,7 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    selector = parsel.Selector(text=html_content)
+    selector = parsel.Selector(html_content)
     url = selector.css("h2.entry-title a::attr(href)").getall()
 
     return url
@@ -26,7 +26,7 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    selector = parsel.Selector(text=html_content)
+    selector = parsel.Selector(html_content)
     next_page_link = selector.css("a.next::attr(href)").get()
 
     if next_page_link:
@@ -37,7 +37,33 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css(".entry-header-inner h1.entry-title::text").getall()
+    title = "".join(title).strip()
+    timestamp = selector.css("ul.post-meta li.meta-date::text").get()
+    writer = selector.css(
+      "ul.post-meta li.meta-author span.author a::text").get()
+    comments_count = selector.css(
+      "div.post-comments h5.title-block::text").re_first(r"\d")
+    summary = selector.css(
+      "div.entry-content > p:first-of-type *::text").getall()
+    summary = "".join(summary).strip()
+    tags = selector.css("section.post-tags li a[rel=tag]::text").getall()
+    category = selector.css("div.meta-category a span.label::text").get()
+
+    result = {
+      "url": url,
+      "title": title,
+      "timestamp": timestamp,
+      "writer": writer,
+      "comments_count": comments_count or 0,
+      "summary": summary,
+      "tags": tags,
+      "category": category
+    }
+
+    return result
 
 
 # Requisito 5
