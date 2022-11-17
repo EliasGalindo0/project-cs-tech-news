78110a12de9ea4b854d608697bbcb8b2ceabb0f6
Elias <eliasgalindo@gmail.com>
2022-11-17 00:24:51 -0300

    feat: ratings

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,32 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    sorted_by_comments = sorted(
+        news, key=lambda x: x["comments_count"], reverse=True
+    )
+
+    return [
+        (content["title"], content["url"])
+        for content in sorted_by_comments][:5]
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+
+    categories = {}
+
+    for item in news:
+        if item["category"] not in categories:
+            categories[item["category"]] = 1
+        else:
+            categories[item["category"]] += 1
+
+    top_5_categ = sorted(
+        categories.items(), key=lambda x: (-x[1], x[0])
+    )
+
+    return [category[0] for category in top_5_categ]
